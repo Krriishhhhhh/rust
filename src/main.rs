9f8e6a2bf7c60f5443dd5a01133a273245568756
Krish <krishgargakio@gmail.com>
2025-09-04 22:44:24 +0530

    chrono crate implemented

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
+use chrono::Local;
+
 fn main() {
-    let x : i32 = -67;
-    let y : u32 = 1000;
-    let z : f32 = 1000.111;
-
-    // Var created after 1st commit 
-    let w : i64 = 1001010101;
-
-
-    // Var created after created a new branch 
-    let z : u128 = 12434114134;
+    let now = Local::now();
+    println!("Current time is : {}", now);
 }
