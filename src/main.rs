96d6741b7836736e0135f79e88440fc4d18bbfe4
Krish <krishgargakio@gmail.com>
2025-09-05 15:19:56 +0530

    Ownership and movability

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
-use chrono::Local;
 
-fn main() {
-    let now = Local::now();
-    println!("Current time is : {}", now);
+fn create_string() {
+    let s1: String = String ::from("Hello");
+    let s2: String = s1;
+
+    // println!("{}" , s1); Can't be done as we moved the owner 
+
+    println!("{}" , s2); //Will print hello
 }
+fn main() {
+    create_string()
+}
+
+//In this code , at first s1 was the owner of "Hello" in the heap , and then we made s2 as the owner of heap
